e4b7dc7d566e052ecaf3e0d9094ec432e71e3fe2
T1meLord1 <vlad.bobrenok.2001@gmail.com>
2024-01-18 10:54:59 +0300

    Добавлена функция для создания массива, перенесён класс Main в одну папку с функциями

---
+++ HW8/Task1/src/functions.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class functions {
+    public static void createMass() {
+       Scanner scanner = new Scanner(System.in);
+       int razmer = scanner.nextInt();
+        int[] mass = new int[razmer];
+        for (int i = 0; i < mass.length ; i++) {
+            int chislo = scanner.nextInt();
+            mass[i] = chislo;
+        }
+        System.out.println(Arrays.toString(mass));
+    }
+    }
+        //#1 Создать массив из 10 целых чисел. Значения могут быть любыми!
+        //todo
+
+        //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
+        //todo
+
+        //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
+        //todo
+
+        //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
+        //todo
+
+
