7feaeedcf51d7ca8c99daf3fe2fd91f55fce35f3
T1meLord1 <vlad.bobrenok.2001@gmail.com>
2024-01-18 12:19:39 +0300

    Добавлено решение для поиска среднего веса всех людей с помощью создания функции weightOfThePeple

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         //todo код программы писать тут!
 //   functions.createMass();
-        int[] mass = functions.createMass();
-        functions.printMassFromEnd(mass);
+//        int[] mass = functions.createMass();
+//        functions.printMassFromEnd(mass);
+        functions.weightOfThePeople();
     }
 }
--- HW8/Task1/src/functions.java
+++ HW8/Task1/src/functions.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class functions {
@@ -19,15 +20,32 @@ public class functions {
         System.out.println(Arrays.toString(mass));
         return mass;
     }
+    //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
+    //todo
     public static void printMassFromEnd(int[] mass){
         for (int i = mass.length-1; i >=0 ; i--) {
             System.out.print(mass[i]+ " ");
         }
     }
-        //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
+        //#4 *** Задание особой сложности. Написать программу, которая сгенерирует случайный вес 10 людям в пределах
+    // от 40 до 100 кг.(Нужно самостоятельно найти информацию, как генерировать числа в заданном диапазоне).
+    //
+    //Далее вам нужно будет вычислить несколько показателей:
+    //1. Средний вес всех людей.
+    //2. Количество людей у которых вес находится в промежутке от 60 до 80 кг включительно. .
         //todo
-
-        //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
-        //todo
+    public static void weightOfThePeople(){
+        int people = 10;
+        Random random = new Random();
+        int allWeight = 0;
+        int averageWeight = 0;
+        for (int i = 1; i <= people ; i++) {
+        int weight = 40 + random.nextInt(61);
+            System.out.println("Вес человека " + (i) +": " + weight + " кг");
+            allWeight += weight;
+        }
+    averageWeight = allWeight / people;
+        System.out.println("Средний вес всех " + people +" людей: " + averageWeight);
+    }
 }
 
