7b104a3aa7fa30fac6e4897670c847c83afc7fbc
T1meLord1 <vlad.bobrenok.2001@gmail.com>
2024-01-18 11:58:54 +0300

    Решена задача 3, добавлена функция printMassFromEnd берущая значения массива из функции createMass

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         //todo код программы писать тут!
-
+//   functions.createMass();
+        int[] mass = functions.createMass();
+        functions.printMassFromEnd(mass);
     }
 }
--- HW8/Task1/src/functions.java
+++ HW8/Task1/src/functions.java
@@ -2,29 +2,32 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class functions {
-    public static void createMass() {
+    //#1 Создать массив из 10 целых чисел. Значения могут быть любыми!
+    //todo
+    //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
+    //todo
+    public static int[] createMass() {
         System.out.println("Введите размерность массива");
         Scanner scanner = new Scanner(System.in);
         int razmer = scanner.nextInt();
         int[] mass = new int[razmer];
-        System.out.println("Заполните массив размерностью" + razmer);
+        System.out.println("Заполните массив размерностью " + razmer);
         for (int i = 0; i < mass.length ; i++) {
             int chislo = scanner.nextInt();
             mass[i] = chislo;
         }
         System.out.println(Arrays.toString(mass));
+        return mass;
     }
+    public static void printMassFromEnd(int[] mass){
+        for (int i = mass.length-1; i >=0 ; i--) {
+            System.out.print(mass[i]+ " ");
+        }
     }
-        //#1 Создать массив из 10 целых чисел. Значения могут быть любыми!
-        //todo
-
-        //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
-        //todo
-
         //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
         //todo
 
         //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
         //todo
-
+}
 
